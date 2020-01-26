577c7f96abc7901c7cb0d8d6f915178bbe666ead
RobbieClarken <robbie.clarken@gmail.com>
2020-01-26 10:12:06 +0000

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -45,6 +45,22 @@ impl Universe {
         self.height
     }
 
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = FixedBitSet::with_capacity((width * self.height) as usize);
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = FixedBitSet::with_capacity((self.width * height) as usize);
+    }
+
     pub fn cells(&self) -> *const u32 {
         self.cells.as_slice().as_ptr()
     }
@@ -103,4 +119,21 @@ impl Universe {
 
         self.cells = next;
     }
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &fixedbitset::FixedBitSet {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+
 }
